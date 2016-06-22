7c38a94859fe42fda93203df52022b0397eab865
Erin Anthony <erinmadisonanthony@gmail.com>
2016-06-22 00:31:27 +0000

    connect form to database

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params) 
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
